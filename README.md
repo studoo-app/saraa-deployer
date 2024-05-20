cb8379a4473c589d33b83a3c1c98073d150f300e
JPechberty <j.pechberty@gmail.com>
2024-05-20 16:16:56 +0200

    Init repo

---
+++ README.md
@@ -0,0 +1,1 @@
+# SARAA stack
---
+++ install/databases/02-gitea_db.sql
@@ -0,0 +1,26 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : database:3306
+-- Généré le : lun. 20 mai 2024 à 12:56
+-- Version du serveur : 11.3.2-MariaDB-1:11.3.2+maria~ubu2204
+-- Version de PHP : 8.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `saraa2_db`
+--
+CREATE DATABASE IF NOT EXISTS `gitea` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
